e81809c73608f1fb56d9ebdd257a22340c5cc555
masontrang <mt9260@gmail.com>
2022-02-21 21:47:39 -0500

    User validation

--- src/main/java/org/launchcode/spaday/models/User.java
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -1,19 +1,38 @@
 package org.launchcode.spaday.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Size;
+
 public class User {
+    @NotBlank(message="username is required")
+    @Size(min=5, max=15)
     private String username;
+
+    @Email
     private String email;
+
+    @NotBlank(message="password is required")
+    @Size(min=6, message="mininmum password length is 6 characters")
     private String password;
+
+    private String verify;
+
+    @Null(message="passwords do not match")
+    @Size(min=6, message="mininmum password length is 6 characters")
+    private String verifyPassword;
 
     public User() {
 
     }
 
-    public User(String username, String email, String password) {
+    public User(String username, String email, String password, String verifyPassword) {
         this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
     }
 
     public String getUsername() {
@@ -38,5 +57,31 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+//        checkPassword();
+
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
+
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    private void checkPassword() {
+        if (!this.password.isEmpty()
+                && (this.getVerifyPassword() != null)
+                && (!this.password.equals(this.verifyPassword))) {
+            verifyPassword = null;
+        }
+
     }
 }
